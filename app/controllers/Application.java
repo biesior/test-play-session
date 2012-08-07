bc3f39af18a122137129a91ec86ef834ba09a61b
Marcus Biesioroff <marcus@biesioroff.com>
2012-08-07 23:19:01 +0200

    Compared to cookies

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,18 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import views.html.index;
 
 public class Application extends Controller {
+
+    private static final int ONE_MINUTE = 60;
+    private static final int ONE_HOUR = ONE_MINUTE * 60;
+    private static final int ONE_DAY = ONE_HOUR * 24;
+    private static final int ONE_YEAR = ONE_DAY * 365;
+
+    private static final int COOKIE_LIFETIME = ONE_YEAR;
 
     public static Result index() {
 
@@ -13,8 +21,15 @@ public class Application extends Controller {
         if (sessionValue == null) {
             sessionValue = "(the session value is null)";
         }
+        Http.Cookie cookie = request().cookies().get("keepcookie");
 
-        return ok(index.render(sessionValue));
+
+        String cookieValue = (cookie == null)
+                ? "(the cookie value is null)"
+                : cookie.value();
+
+
+        return ok(index.render(sessionValue, cookieValue));
     }
 
     public static Result set() {
@@ -24,6 +39,18 @@ public class Application extends Controller {
 
     public static Result destroy() {
         session().remove("keepme");
+        return redirect(routes.Application.index());
+    }
+
+    public static Result setCookie() {
+
+        response().setCookie("keepcookie", "This is cookie set as... cookie, for " + COOKIE_LIFETIME + " seconds", COOKIE_LIFETIME);
+        return redirect(routes.Application.index());
+    }
+
+    public static Result destroyCookie() {
+
+        response().discardCookies("keepcookie");
         return redirect(routes.Application.index());
     }
 
